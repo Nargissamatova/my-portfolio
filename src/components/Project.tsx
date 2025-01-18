9bcc8914dbcaad8184a82f8424f9756e718b3351
Nargis <nargissamatova16@gmail.com>
2025-01-18 23:48:09 +0200

    Updates

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,13 +4,19 @@ import mock02 from "../assets/images/countriesapp.png";
 import mock03 from "../assets/images/tictactoeapp.png";
 import mock04 from "../assets/images/recipesapp.png";
 
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 function Project() {
   return (
     <div className="projects-container" id="projects">
       <h1>Projects</h1>
       <div className="projects-grid">
         <div className="project">
-          <a href="" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/Nargissamatova/Druid_Partnering_Project_React24k"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={mock01} className="zoom" alt="thumbnail" width="100%" />
           </a>
           <a href="" target="_blank" rel="noreferrer">
@@ -25,7 +31,30 @@ function Project() {
           </p>
         </div>
         <div className="project">
+          <a
+            href="https://github.com/Nargissamatova/food_recipe_app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={mock04} className="zoom" alt="thumbnail" width="100%" />
+          </a>
           <a href="" target="_blank" rel="noreferrer">
+            <h2>Digital Cookbook</h2>
+          </a>
+          <p>
+            Discover detailed recipes with ingredients, step-by-step
+            instructions, and nutritional info, all sourced from a recipe API.
+            Search by keyword, filter by cuisine, or customize results to match
+            your dietary needs. From quick meals to bold new flavors, this app
+            turns recipe exploration into an enjoyable experience.
+          </p>
+        </div>
+        <div className="project">
+          <a
+            href="https://github.com/Nargissamatova/countries-bootstrap-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={mock02} className="zoom" alt="thumbnail" width="100%" />
           </a>
           <a href="" target="_blank" rel="noreferrer">
@@ -40,10 +69,18 @@ function Project() {
           </p>
         </div>
         <div className="project">
-          <a href="" target="_blank" rel="noreferrer">
+          <a
+            href="https://tic-tac-toe-reactapplication.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={mock03} className="zoom" alt="thumbnail" width="100%" />
           </a>
-          <a href="" target="_blank" rel="noreferrer">
+          <a
+            href="https://github.com/Nargissamatova/tic-tac-toe"
+            target="_blank"
+            rel="noreferrer"
+          >
             <h2>Tic Tac Toe</h2>
           </a>
           <p>
@@ -52,19 +89,16 @@ function Project() {
             effects that enhance the experience for every move, win, or loss.
           </p>
         </div>
-        <div className="project">
-          <a href="" target="_blank" rel="noreferrer">
-            <img src={mock04} className="zoom" alt="thumbnail" width="100%" />
-          </a>
-          <a href="" target="_blank" rel="noreferrer">
-            <h2>Digital Cookbook</h2>
-          </a>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem rerum recusandae eligendi harum adipisci quas sit quo
-            architecto perferendis ratione?
-          </p>
-        </div>
+      </div>
+      <div className="github-button-container">
+        <a
+          href="https://github.com/Nargissamatova?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="github-button"
+        >
+          <span>See more</span> <GitHubIcon />
+        </a>
       </div>
     </div>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Timeline } from "./Timeline";
 export { default as Navigation } from "./Navigation";
 export { default as Footer } from "./Footer";
 export { default as Contact } from "./Contact";
+export { default as Project } from "./Project";
